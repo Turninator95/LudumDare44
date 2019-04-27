0e14f8a1a999f6d656abdbffd142d4fb22881c1f
valexent <47042660+valexent@users.noreply.github.com>
2019-04-27 17:23:57 +0200

    this is healthbar

---
+++ Assets/Sprites/HealthBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class HealthBar : MonoBehaviour
+{
+    public Image healthBar, healthBar2;
+    public float maxHealth = 100f;
+    [Range(0.1f, 100f)]
+    public float health;
+    public float shotDamage = 10f;
+
+    void Start()
+    {
+        healthBar = GetComponent<Image>();
+        health = maxHealth;
+    }
+
+    void Update()
+    {
+        healthBar.fillAmount = health / maxHealth;
+        healthBar2.fillAmount = (health - shotDamage) / maxHealth;
+
+
+    }
+}
