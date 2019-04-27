4bceb1ac4fa955678aa4803c4fe4a9b4d028353c
Daniel Turner <dturner@th-koeln.de>
2019-04-27 13:55:07 +0200

    Improved code readability
    +fixed inifite shooting

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,11 +13,6 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.forward * Time.deltaTime;
-    }
-
-    private void OnBecameInvisible()
-    {
-        Destroy(gameObject);
+        
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,47 +4,83 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("References"),SerializeField]
+    private GameObject scope;
     [SerializeField]
-    private GameObject scope, bullet, gun;
+    private GameObject bullet, gun;
     [SerializeField, Range(1.0f, 20.0f)]
+    [Header("Variables")]
     private float runningSpeed = 1;
     private bool bulletFired = false;
     [SerializeField]
     private float fireTimeout = 5f;
+
+    [SerializeField]
+    private int initialAmmo = 20, maxAmmo = 100, costPerShot = 1;
+    private int currentAmmo;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+        currentAmmo = initialAmmo;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Time.deltaTime * runningSpeed;
+        ProcessMovementInput();
+        ProcessAimingInput();
+        ProcessShooting();
 
+        if (currentAmmo <= 0)
+        {
+            Debug.Log("YOU DED SON");
+        }
+    }
+
+    private void ProcessMovementInput()
+    {
+        transform.position = transform.position + 
+            new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) 
+            * Time.deltaTime 
+            * runningSpeed;
+    }
+    private void ProcessAimingInput()
+    {
         if (Input.GetAxis("HorizontalAim") != 0 || Input.GetAxis("VerticalAim") != 0)
         {
-            scope.transform.position = transform.position + new Vector3(Input.GetAxis("HorizontalAim"), 0, Input.GetAxis("VerticalAim"));
-
+            scope.transform.position = transform.position + 
+                new Vector3(Input.GetAxis("HorizontalAim"), 0, Input.GetAxis("VerticalAim"));
             gun.transform.LookAt(scope.transform);
-            gun.transform.rotation = Quaternion.Euler(90, gun.transform.rotation.eulerAngles.y, gun.transform.rotation.eulerAngles.z);
+            //gun.transform.rotation = Quaternion.Euler(90, gun.transform.rotation.eulerAngles.y, gun.transform.rotation.eulerAngles.z);
 
         }
         else if (Input.GetAxis("HorizontalAim") == 0 && Input.GetAxis("VerticalAim") == 0)
         {
             scope.transform.position = transform.position;
         }
-
+    }
+    private void ProcessShooting()
+    {
         if (Input.GetAxis("Fire1") > 0 && !bulletFired)
         {
-            Instantiate(bullet, transform.position, gun.transform.rotation);
-            bulletFired = true;
-            StartCoroutine(ResetBulletFired());
+            if (currentAmmo - costPerShot > 0)
+            {
+                Instantiate(bullet, transform.position, gun.transform.rotation);
+                bulletFired = true;
+                StartCoroutine(ResetBulletFired());
+                currentAmmo -= costPerShot;
+                Debug.Log(currentAmmo);
+            }
         }
     }
 
-    IEnumerator ResetBulletFired()
+    private IEnumerator ResetBulletFired()
     {
         yield return new WaitForSeconds(fireTimeout);
+        Debug.Log("You can shoot again!!!!!");
         bulletFired = false;
     }
 }
