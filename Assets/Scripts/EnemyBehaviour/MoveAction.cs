8170a6827bfd97af8bd47eab0c5f56f0a427443c
DavidLeertasteW <david.wildemann98@gmail.com>
2019-04-27 16:40:49 +0200

    Changed enemy Movement

--- Assets/Scripts/EnemyBehaviour/MoveAction.cs
+++ Assets/Scripts/EnemyBehaviour/MoveAction.cs
@@ -9,19 +9,27 @@ public class MoveAction : EnemyActions
     private float moveDistance;
     private Vector3 startPosition;
     private bool startPositionSet = false;
+    private Rigidbody rigidbody;
     public override void Execute(Enemy enemy)
     {
+        rigidbody = enemy.gameObject.GetComponent<Rigidbody>();
+        if (rigidbody == null)
+        {
+            rigidbody = enemy.gameObject.AddComponent<Rigidbody>();
+            rigidbody.useGravity = false;
+        }
         if (startPositionSet == false)
         {
-            startPosition = enemy.transform.position;
+            startPosition = rigidbody.position;
             startPositionSet = true;
         }
         if (Vector3.Distance(startPosition, enemy.transform.position) < moveDistance)
         {
-            enemy.transform.position += enemy.transform.forward * Time.deltaTime * enemy.MovementSpeed;
+            rigidbody.velocity = enemy.transform.forward  * enemy.MovementSpeed;
         }
         else
         {
+            rigidbody.velocity = Vector3.zero;
             startPositionSet = false;
             enemy.ActionCompleted();
         }
