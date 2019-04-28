7a3e14fdfee4caea7f5d4bceab65491a568467b5
Daniel Turner <dturner@th-koeln.de>
2019-04-28 10:54:06 +0200

    Added storage option for difficulty level

---
+++ Assets/Scripts/DifficultyLevel.cs
@@ -0,0 +1,7 @@
+﻿[System.Serializable]
+public enum DifficultyLevel
+{
+    TrustFundBaby,
+    MiddleClassHousewife,
+    AmericanDream
+}
---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable, CreateAssetMenu(menuName = "Game Settings")]
+public class GameSettings : ScriptableObject
+{
+    [SerializeField]
+    private DifficultyLevel difficultyLevel;
+    #region Properties
+    public DifficultyLevel DifficultyLevel { get => difficultyLevel; set => difficultyLevel = value; }
+    #endregion
+}
