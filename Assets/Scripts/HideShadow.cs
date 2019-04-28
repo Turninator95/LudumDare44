08cc7aefaea9ce8543ee345f82eafccce519eded
Daniel Turner <dturner@th-koeln.de>
2019-04-28 16:04:02 +0200

    Player HideShadow

---
+++ Assets/Scripts/HideShadow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideShadow : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Wall")
+        {
+            spriteRenderer.enabled = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Wall")
+        {
+            spriteRenderer.enabled = true;
+        }
+    }
+}
