46b9d330e085cff4cae15cd98180b1a901324d84
Daniel Turner <dturner@th-koeln.de>
2019-04-28 16:21:40 +0200

    THE HOT UPDATE DAVID HAS BEEN WAITING FOR. GOTTA SWALLOW THE BITTER PILL OF NO LONGER EXPLOITING THE GAME TO GET THIS TOUGH!!!!!!!

---
+++ Assets/Scripts/PlayerUpgrades/PlayerUpgrade.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public abstract class PlayerUpgrade : ScriptableObject
+{
+    [SerializeField]
+    private int upgradeCost = 1;
+    #region Properties
+    public int UpgradeCost { get => upgradeCost; set => upgradeCost = value; }
+    #endregion
+    public abstract void Process(); 
+}
