f96ead04818c3005443d80c744bfd004bfa088b4
DavidLeertasteW <david.wildemann98@gmail.com>
2019-04-28 15:10:17 +0200

    Made tiles great again

---
+++ Assets/Scripts/SpawnObstaclesOnTilemap.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Tilemaps;
+
+public class SpawnObstaclesOnTilemap : MonoBehaviour
+{
+
+    
+    public Tilemap tilemap;
+    public int layer = 1;
+    public GameObject obstacle; 
+    private void Start()
+    {
+        foreach (var item in tilemap.cellBounds.allPositionsWithin)
+        {
+            Vector3Int localPlace = new Vector3Int(item.x, item.y, item.z);
+            Vector3 place = tilemap.CellToWorld(localPlace);
+            //place = new Vector3(place.x, place.z, place.y);
+            
+            if (tilemap.HasTile(localPlace))
+            {
+                if (localPlace.z == layer)
+                {
+                    //Debug.Log("tile at " + place);
+                    GameObject tmpOnj = GameObject.Instantiate(obstacle);
+                    tmpOnj.transform.position = place + new Vector3(0.5f,0,0.5f);
+                    
+                    
+                }
+            }
+        }
+        
+    }
+
+}
